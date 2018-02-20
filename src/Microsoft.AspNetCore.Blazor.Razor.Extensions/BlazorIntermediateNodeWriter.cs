d99344f8dbcdc31f07a05b04c5b129af69c05ede
Ryan Nowak <nowakra@gmail.com>
2018-02-19 16:16:44 -0800

    Adopt more of Razor Exensibility
    Removes some workarounds and uses Razor extensibility in a few more
    places.
    
    _ViewImports now works in VS

--- src/Microsoft.AspNetCore.Blazor.Razor.Extensions/BlazorIntermediateNodeWriter.cs
+++ src/Microsoft.AspNetCore.Blazor.Razor.Extensions/BlazorIntermediateNodeWriter.cs
@@ -123,7 +123,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
             // text to display
             _scopeStack.IncrementCurrentScopeChildCount(context);
             context.CodeWriter
-                .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(RenderTreeBuilder.AddContent)}")
+                .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(BlazorApi.RenderTreeBuilder.AddContent)}")
                 .Write((_sourceSequence++).ToString())
                 .WriteParameterSeparator();
 
@@ -213,7 +213,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                             // Text node
                             _scopeStack.IncrementCurrentScopeChildCount(context);
                             codeWriter
-                                .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(RenderTreeBuilder.AddContent)}")
+                                .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(BlazorApi.RenderTreeBuilder.AddContent)}")
                                 .Write((_sourceSequence++).ToString())
                                 .WriteParameterSeparator()
                                 .WriteStringLiteral(nextToken.Data)
@@ -234,14 +234,14 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                                 if (isComponent)
                                 {
                                     codeWriter
-                                        .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(RenderTreeBuilder.OpenComponent)}<{componentTypeName}>")
+                                        .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(BlazorApi.RenderTreeBuilder.OpenComponent)}<{componentTypeName}>")
                                         .Write((_sourceSequence++).ToString())
                                         .WriteEndMethodInvocation();
                                 }
                                 else
                                 {
                                     codeWriter
-                                        .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(RenderTreeBuilder.OpenElement)}")
+                                        .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(BlazorApi.RenderTreeBuilder.OpenElement)}")
                                         .Write((_sourceSequence++).ToString())
                                         .WriteParameterSeparator()
                                         .WriteStringLiteral(nextTag.Data)
@@ -291,8 +291,8 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                                     isComponent: isComponent,
                                     source: CalculateSourcePosition(node.Source, nextToken.Position));
                                 var closeMethodName = isComponent
-                                    ? nameof(RenderTreeBuilder.CloseComponent)
-                                    : nameof(RenderTreeBuilder.CloseElement);
+                                    ? nameof(BlazorApi.RenderTreeBuilder.CloseComponent)
+                                    : nameof(BlazorApi.RenderTreeBuilder.CloseElement);
                                 codeWriter
                                     .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{closeMethodName}")
                                     .WriteEndMethodInvocation();
@@ -350,7 +350,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
                 // For any other attribute token (e.g., @onclick(...)), treat it as an expression
                 // that will evaluate as an attribute frame
                 context.CodeWriter
-                    .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(RenderTreeBuilder.AddAttribute)}")
+                    .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(BlazorApi.RenderTreeBuilder.AddAttribute)}")
                     .Write((_sourceSequence++).ToString())
                     .WriteParameterSeparator()
                     .Write(token.AttributeValue.Content)
@@ -429,7 +429,7 @@ namespace Microsoft.AspNetCore.Blazor.Razor
         public void BeginWriteAttribute(CodeWriter codeWriter, string key)
         {
             codeWriter
-                .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(RenderTreeBuilder.AddAttribute)}")
+                .WriteStartMethodInvocation($"{_scopeStack.BuilderVarName}.{nameof(BlazorApi.RenderTreeBuilder.AddAttribute)}")
                 .Write((_sourceSequence++).ToString())
                 .WriteParameterSeparator()
                 .WriteStringLiteral(key)
