af841e7bf8912cb2421ab5a41aee68a0e77b8b3a
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-02-28 10:48:56 +0000

    Enable response compression in "hosted" template

--- src/Microsoft.AspNetCore.Blazor.Templates/content/BlazorHosted.CSharp/BlazorHosted.CSharp.Server/Startup.cs
+++ src/Microsoft.AspNetCore.Blazor.Templates/content/BlazorHosted.CSharp/BlazorHosted.CSharp.Server/Startup.cs
@@ -3,8 +3,11 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Net.Mime;
 
 namespace BlazorHosted.CSharp.Server
 {
@@ -14,15 +17,23 @@ namespace BlazorHosted.CSharp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddJsonOptions(opts =>
+            services.AddMvc().AddJsonOptions(options =>
             {
-                opts.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
+
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes
+                    .Concat(new[] { MediaTypeNames.Application.Octet });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseResponseCompression();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
