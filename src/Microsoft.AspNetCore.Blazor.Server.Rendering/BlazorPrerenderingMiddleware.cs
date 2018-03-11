b8fa1a91f00df1a2b67926c63fa364ebf53c7bcd
Florian Dohrendorf <fdohrendorf@outlook.com>
2018-03-11 20:06:35 +0100

    prerendering hello world using index.html content

--- src/Microsoft.AspNetCore.Blazor.Server.Rendering/BlazorPrerenderingMiddleware.cs
+++ src/Microsoft.AspNetCore.Blazor.Server.Rendering/BlazorPrerenderingMiddleware.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AngleSharp;
@@ -61,28 +63,76 @@ namespace Microsoft.AspNetCore.Blazor.Server.Rendering
             });
         }
 
-        private static string Prerender(string html)
+        private static string Prerender(string htmlTemplate)
         {
-            var tokenizer = new HtmlTokenizer(new TextSource(html), HtmlEntityService.Resolver);
+            // copied from IndexHtmlFileProvider.cs
+            var resultBuilder = new StringBuilder();
 
+            // Search for a tag of the form <app></app>, and replace
+            // it with the prerendered content
+            var tokenizer = new HtmlTokenizer(
+                new TextSource(htmlTemplate), 
+                HtmlEntityService.Resolver);
+            var currentRangeStartPos = 0;
+            var isInBlazorBootTag = false;
+            var resumeOnNextToken = false;
             while (true)
             {
                 var token = tokenizer.Get();
+                if (resumeOnNextToken)
+                {
+                    resumeOnNextToken = false;
+                    currentRangeStartPos = token.Position.Position;
+                }
+
                 switch (token.Type)
                 {
+                    case HtmlTokenType.StartTag:
+                        {
+                            // Only do anything special if this is a Blazor boot tag
+                            var tag = token.AsTag();
+                            if (IsBlazorBootTag(tag))
+                            {
+                                // First, emit the original source text prior to this special tag, since
+                                // we want that to be unchanged
+                                resultBuilder.Append(htmlTemplate, currentRangeStartPos, token.Position.Position - currentRangeStartPos - 1);
+
+                                // Instead of emitting the source text for this special tag, emit a fully-
+                                // configured Blazor boot script tag
+                                AppendScriptTagWithBootConfig(resultBuilder);
+
+                                // Set a flag so we know not to emit anything else until the special
+                                // tag is closed
+                                isInBlazorBootTag = true;
+                            }
+                            break;
+                        }
+
+                    case HtmlTokenType.EndTag:
+                        // If this is an end tag corresponding to the Blazor boot script tag, we
+                        // can switch back into the mode of emitting the original source text
+                        if (isInBlazorBootTag)
+                        {
+                            isInBlazorBootTag = false;
+                            resumeOnNextToken = true;
+                        }
+                        break;
+
                     case HtmlTokenType.EndOfFile:
-                        return @"<!DOCTYPE html>
-<html>
-<head>
-    <meta charset=""utf-8"" />
-    <title>Sample Blazor app</title>
-</head>
-<body>
-    <h1>Hello prerendering</h1>
-</body>
-</html>";
+                        // Finally, emit any remaining text from the original source file
+                        resultBuilder.Append(htmlTemplate, currentRangeStartPos, htmlTemplate.Length - currentRangeStartPos);
+                        return resultBuilder.ToString();
                 }
             }
+        }
+
+        private static bool IsBlazorBootTag(HtmlTagToken tag)
+            => string.Equals(tag.Name, "app", StringComparison.Ordinal);
+
+        private static void AppendScriptTagWithBootConfig(
+            StringBuilder resultBuilder)
+        {
+            resultBuilder.AppendLine("<h1>Hello Prerendering</h1>");
         }
     }
 
