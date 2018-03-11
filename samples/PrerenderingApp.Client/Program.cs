948bf9d22cc88f8221f9084699ea9cd92ff5cd69
Florian Dohrendorf <fdohrendorf@outlook.com>
2018-03-11 12:05:35 +0100

    move prerendering to its own project

---
+++ samples/PrerenderingApp.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Browser.Http;
+using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.Rendering;
+using Microsoft.AspNetCore.Blazor.RenderTree;
+using Microsoft.AspNetCore.Blazor.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PrerenderingApp.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new BrowserRenderer().AddComponent<Home>("app");
+        }
+
+        /*static void Server(string[] args)
+        {
+            var renderer = new PreRenderer();//.AddComponent<Home>("app");
+            renderer.DoStuff<Home>();
+        }*/
+    }
+}
