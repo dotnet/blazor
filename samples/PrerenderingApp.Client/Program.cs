459d0f1daf494b30223ec9d4c96bf63e7ae0ff1f
Florian Dohrendorf <fdohrendorf@outlook.com>
2018-03-12 23:39:24 +0100

    use different implementations of service while prerendering to skip http call

--- samples/PrerenderingApp.Client/Program.cs
+++ samples/PrerenderingApp.Client/Program.cs
@@ -1,18 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using Microsoft.AspNetCore.Blazor.Browser.Http;
 using Microsoft.AspNetCore.Blazor.Browser.Rendering;
 using Microsoft.AspNetCore.Blazor.Browser.Services;
-using Microsoft.AspNetCore.Blazor.Components;
-using Microsoft.AspNetCore.Blazor.Rendering;
-using Microsoft.AspNetCore.Blazor.RenderTree;
-using Microsoft.AspNetCore.Blazor.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PrerenderingApp.Client
 {
@@ -23,6 +14,7 @@ namespace PrerenderingApp.Client
             var serviceProvider = new BrowserServiceProvider(configure =>
             {
                 configure.AddSharedServices();
+                configure.AddSingleton<IWeatherService, ClientWeatherService>();
             });
 
             new BrowserRenderer(serviceProvider).AddComponent<Home>("app");
---
+++ samples/PrerenderingApp.Server/ServerWeatherService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrerenderingApp.Client;
+
+namespace PrerenderingApp.Server
+{
+    public class ServerWeatherService : IWeatherService
+    {
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<IEnumerable<WeatherForecast>> GetForecast()
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }));
+        }
+    }
+}
