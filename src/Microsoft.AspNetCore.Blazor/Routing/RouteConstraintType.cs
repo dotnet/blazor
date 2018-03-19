11e8670cbe975f2692261fd203b4e70655a794c1
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-03-19 15:26:19 +0000

    Support route constraints of the form ":type" (e.g, ":int", ":guid", etc.)

---
+++ src/Microsoft.AspNetCore.Blazor/Routing/RouteConstraintType.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Blazor.Routing
+{
+    internal class RouteConstraintType<T> : RouteConstraint
+    {
+        public delegate bool TryParseDelegate(string str, out T result);
+
+        private readonly TryParseDelegate _parser;
+
+        public RouteConstraintType(TryParseDelegate parser)
+        {
+            _parser = parser;
+        }
+
+        public override bool Match(string pathSegment, out object convertedValue)
+        {
+            if (_parser(pathSegment, out var result))
+            {
+                convertedValue = result;
+                return true;
+            }
+            else
+            {
+                convertedValue = null;
+                return false;
+            }
+        }
+    }
+}
