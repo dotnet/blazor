d0d2772c98063a5be14c595ee11ccb92d099c844
Ryouko Konpaku <ryoukoasakura18@gmail.com>
2018-04-28 04:05:01 +0800

    Move Property Naming Enum to SimpleJson namespace.

--- src/Microsoft.AspNetCore.Blazor/Json/JsonUtil.cs
+++ src/Microsoft.AspNetCore.Blazor/Json/JsonUtil.cs
@@ -13,7 +13,7 @@ namespace Microsoft.AspNetCore.Blazor
     public static class JsonUtil
     {
         /// <summary>
-        /// Serializes the value as a JSON string using the default property naming scheme <see cref="PropertyNaming.CamelCase"/>.
+        /// Serializes the value as a JSON string using the default property naming scheme <see cref="SimpleJson.PropertyNaming.CamelCase"/>.
         /// </summary>
         /// <param name="value">The value to serialize.</param>
         /// <returns>The JSON string.</returns>
@@ -26,8 +26,8 @@ namespace Microsoft.AspNetCore.Blazor
         /// <param name="value">The value to serialize.</param>
         /// <param name="naming">The property naming scheme for serializing.</param>
         /// <returns>The JSON string.</returns>
-        public static string Serialize(object value, PropertyNaming naming)
-            => SimpleJson.SimpleJson.SerializeObject(value, (int)naming);
+        public static string Serialize(object value, SimpleJson.PropertyNaming naming)
+            => SimpleJson.SimpleJson.SerializeObject(value, naming);
 
         /// <summary>
         /// Deserializes the JSON string, creating an object of the specified generic type.
@@ -45,22 +45,7 @@ namespace Microsoft.AspNetCore.Blazor
         /// <param name="json">The JSON string.</param>
         /// <param name="propertyNaming">The property naming of the JSON string.</param>
         /// <returns>An object of the specified type.</returns>
-        public static T Deserialize<T>(string json, PropertyNaming propertyNaming)
-            => SimpleJson.SimpleJson.DeserializeObject<T>(json, (int)propertyNaming);
-
-        /// <summary>
-        /// Property Naming scheme to be used by simple json when deserializing or serializing JSON.
-        /// </summary>
-        public enum PropertyNaming
-        {
-            /// <summary>
-            /// Camel case JSON property naming scheme.
-            /// </summary>
-            CamelCase = 1,
-            /// <summary>
-            /// Pascal case JSON property naming scheme.
-            /// </summary>
-            PascalCase = 2
-        }
+        public static T Deserialize<T>(string json, SimpleJson.PropertyNaming propertyNaming)
+            => SimpleJson.SimpleJson.DeserializeObject<T>(json, propertyNaming);
     }
 }
