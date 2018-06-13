bb185618ebe752aaebbfeaedcd64e7e09cbf0adb
Carmelo Messina <carmelo.messina@uazo.it>
2018-06-13 11:08:59 +0200

    test publish

---
+++ deploy.cmd
@@ -0,0 +1,4 @@
+d:
+cd D:\Progetti\Uazo\Blazor-Uazo\src\Microsoft.AspNetCore.Blazor\bin\Debug
+
+dotnet nuget push Uazo.AspNetCore.Blazor.Reload.0.4.0-preview1-t000.nupkg -k oy2dosmfr5q3zjy3tbmazifhjkwwxkzezihywmzx35g7eq -s https://api.nuget.org/v3/index.json
