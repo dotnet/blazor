6a29bdde38c1dc04274cc8de5f91419b87021253
Stefan Nikolei <stefan@nikolei.eu>
2018-06-15 21:47:56 +0200

    minify blazor.js

--- src/Microsoft.AspNetCore.Blazor.Browser.JS/webpack.config.js
+++ src/Microsoft.AspNetCore.Blazor.Browser.JS/webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
     module: {
         rules: [{ test: /\.ts?$/, loader: 'ts-loader' }]
     },
-    entry: { 'blazor': './src/Boot.ts' },
-    output: { path: path.join(__dirname, '/dist'), filename: '[name].js' }
+  entry: {
+    'blazor': './src/Boot.ts',
+    'blazor.min': './src/Boot.ts',
+  },
+    output: { path: path.join(__dirname, '/dist'), filename: '[name].js' },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      include: /\.min\.js$/,
+      compress: {
+        warnings: false
+      },
+    })
+  ]
 };
