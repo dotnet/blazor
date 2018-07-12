128cf5fcb6a4d3427f4b9f5ef24f3d3af40bf793
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-07-12 10:52:33 +0100

    Add build command for generating the new boot JSON file

--- src/Microsoft.AspNetCore.Blazor.Build/Core/IndexHtmlWriter.cs
+++ src/Microsoft.AspNetCore.Blazor.Build/Core/IndexHtmlWriter.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+// TODO: Remove AngleSharp dependency from this project once we remove IndexHtmlWriter
+
 using AngleSharp;
 using AngleSharp.Extensions;
 using AngleSharp.Html;
