705ef32ad11a21c6c6bb8551f5be4c23a0443add
Tony Alexander Hild <tony_hild@yahoo.com>
2018-07-19 17:37:29 -0300

    Fixed exporting platform to window object

--- src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Environment.ts
+++ src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Environment.ts
@@ -2,10 +2,12 @@
 // so that consumers can be agnostic about which implementation they use.
 // Basic alternative to having an actual DI container.
 import { Platform } from './Platform/Platform';
+import { exportBlazor } from './GlobalExports';
 
 export let platform: Platform;
 
 export function setPlatform(platformInstance: Platform) {
   platform = platformInstance;
+  exportBlazor();
   return platform;
 }
